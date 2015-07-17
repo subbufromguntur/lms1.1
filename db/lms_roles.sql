4841507867f56f7414d24d1e07299b0e62524e43
subbufromguntur <subburamanu@yahoo.co.in>
2015-07-17 14:15:55 +0530

    changed database ip

--- db/lms_roles.sql
+++ db/lms_roles.sql
@@ -1,3 +1,10 @@
+create table lms_roles (
+        role_id int(10),
+        group_id int(10),
+        name varchar(1000),
+        permission_list varchar(1000)
+)
+
 insert into lms_roles(role_id, group_id, name, permission_list) values
 (1, 9000, 'L1 Admin', '2001,2002,2003,6501'),
 (2, 9001, 'L1 User', '2001,2002,2003,6501'),
